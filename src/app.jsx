a94fad96f073e749b9617463b28160ba4efee250
zxh <zhangxinghai79@gmail.com>
2019-01-10 00:58:26 +0800

    TRY COMPONENT

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import { Com } from './com'
 
-const element = <h3>Learn React</h3>
+const ref = React.createRef()
+const element =
+<div ref={ref}>
+  <h3>Learn React</h3>
+  <p>
+    In order to accept your pull request, we need you to submit a CLA. You only need to do this once, so if you’ve done this for another Facebook open source project, you’re good to go. If you are submitting a pull request for the first time, just let us know that you have completed the CLA and we can cross-check with your GitHub username.
+  </p>
+  <Com name="Singhi John" age={30}/>
+</div>
 
 export default element
