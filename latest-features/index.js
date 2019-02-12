973d33c9da4232380734705f83f6ddeccf398565
Singhi John <zhangxinghai79@gmail.com>
2019-02-13 00:13:42 +0800

    home

--- latest-features/index.js
+++ latest-features/index.js
@@ -3,3 +3,17 @@ import * as ReactDOM from 'react-dom'
 import App from './app'
 
 ReactDOM.render(<App/>, document.getElementById('reactApp'))
+
+// macrotasks: setTimeout, setInterval, setImmediate, requestAnimationFrame, I/O, UI rendering
+// microtasks: process.nextTick, Promises, Object.observe, MutationObserver
+
+setTimeout(() => {
+  console.log('setTimeout')
+}, 0)
+Promise.resolve().then(() => {
+  console.log('Promise')
+})
+requestAnimationFrame(() => {
+  console.log('requestAnimationFrame')
+})
+console.log('Script')
--- latest-features/suspense.js
+++ latest-features/suspense.js
@@ -2,7 +2,19 @@ import * as React from 'react'
 const RemoteComponent = React.lazy(() => import('./remote-component'));
 
 export default (props) => {
-  return <div>
+  const [sum, dispatch] = React.useReducer((pre, next) => {
+    const rand = (Math.random() * 100000000).toString(16).substr(0,6)
+    return rand
+  }, '98bace')
+  const alphaSum = React.useMemo(() => {
+    // console.log('sum is changed')
+    return `#${sum}11`
+  }, [sum])
+  // console.log(alphaSum)
+  return <div style={{border: '1px solid #DDD', padding: '12px'}} onClick={() => {
+    dispatch({ payload: 90 })
+  }}>
+    <h1 style={{backgroundColor: alphaSum}}>{sum}</h1>
     <RemoteComponent name={props.name}/>
   </div>
 }
